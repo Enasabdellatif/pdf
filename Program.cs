6ee562188b3f15a849d4d3152a691419813ded7c
Enasabdellatif <70613289+Enasabdellatif@users.noreply.github.com>
2024-08-13 10:40:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using RestaurantBookingReport.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+// Register the custom services for dependency injection
+builder.Services.AddSingleton<RestaurantService>();
+builder.Services.AddSingleton<PdfService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
